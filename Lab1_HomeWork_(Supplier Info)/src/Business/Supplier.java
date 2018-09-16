38192ba6ebe2687ac1ad1f77f814f12552d7d957
Saurabh Nitin Gujare <gujare.s@husky.neu.edu>
2018-09-16 19:17:56 -0400

    Adding Lab1_Homework_(Supplier Info)

---
+++ Lab1_HomeWork_(Supplier Info)/src/Business/Supplier.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author saurabhgujare
+ */
+public class Supplier {
+    private String suppliername;
+    private String supplier_phone;
+    private String supplier_email;
+
+    public String getSuppliername() {
+        return suppliername;
+    }
+
+    public void setSuppliername(String suppliername) {
+        this.suppliername = suppliername;
+    }
+
+    public String getSupplier_phone() {
+        return supplier_phone;
+    }
+
+    public void setSupplier_phone(String supplier_phone) {
+        this.supplier_phone = supplier_phone;
+    }
+
+    public String getSupplier_email() {
+        return supplier_email;
+    }
+
+    public void setSupplier_email(String supplier_email) {
+        this.supplier_email = supplier_email;
+    }
+    
+    
+}
